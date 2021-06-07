b561a28a20da5c44caf1028a7563183532937f9c
rakib <phi.rakib@gmail.com>
2021-06-07 22:40:07 +0600

    set initial state, added getters to get todos, loading, error, added fetchTodo action and state change logic aftr data fetching

--- src/store/modules/Todo.js
+++ src/store/modules/Todo.js
@@ -1,10 +1,45 @@
-const state = {};
+import axios from "axios";
 
-const getters = {};
+const state = {
+  todos: [],
+  loading: false,
+  error: null,
+};
 
-const actions = {};
+const getters = {
+  allTodos: (state) => state.todos,
+  loading: (state) => state.loading,
+  error: (state) => state.error,
+};
 
-const mutations = {};
+const actions = {
+  async fetchTodos({ commit }) {
+    commit("setLoading", true);
+    commit("setError", null);
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      commit("setTodos", response.data);
+    } catch (error) {
+      commit("setError", error.message);
+    } finally {
+      commit("setLoading", false);
+    }
+  },
+};
+
+const mutations = {
+  setTodos: (state, todos) => {
+    state.todos = todos;
+  },
+  setError: (state, error) => {
+    state.error = error;
+  },
+  setLoading: (state, payload) => {
+    state.loading = payload;
+  },
+};
 
 export default {
   state,
