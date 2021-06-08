980c211f4f816457e8e634d982fbfcfb447c7cd8
rakib <phi.rakib@gmail.com>
2021-06-08 22:48:45 +0600

    created todo delete component and the necessary functionalities

---
+++ src/components/DeleteTodo.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="deleteTodo(this.id)">Delete</button>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "DeleteTodo",
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapActions(["deleteTodo"]),
+  },
+};
+</script>
