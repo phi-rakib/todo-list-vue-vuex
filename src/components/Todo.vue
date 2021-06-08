a835d88c0feaffe2a6a0291f782d3dfca8c2ac57
rakib <phi.rakib@gmail.com>
2021-06-08 22:49:37 +0600

    added todo delete component to delete todo by clicking the delete button

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,15 +2,20 @@
   <div>
     <div v-for="todo in allTodos" :key="todo.id">
       <span :class="{ job_done: todo.completed }">{{ todo.title }}</span>
+      <DeleteTodo :id="todo.id" />
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
+import DeleteTodo from "./DeleteTodo.vue";
 
 export default {
   name: "Todo",
+  components: {
+    DeleteTodo
+  },
   computed: { ...mapGetters(["allTodos"]) },
   methods: {
     ...mapActions(["fetchTodos"]),
