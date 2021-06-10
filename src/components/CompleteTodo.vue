f506d938ff4c219bb1ddb2b4f584ae7e3fc7bd8c
rakib <phi.rakib@gmail.com>
2021-06-10 23:24:46 +0600

    created completed todo component with functionality

---
+++ src/components/CompleteTodo.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="checkbox" @change="todoUpdate" />
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "CompleteTodo",
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapActions(["updateTodo"]),
+    todoUpdate(event) {
+      event.preventDefault();
+      const todo = { ...this.todo };
+      todo.completed = !todo.completed;
+      this.updateTodo(todo);
+    },
+  },
+};
+</script>
