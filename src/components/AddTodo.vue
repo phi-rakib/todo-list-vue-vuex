15475bac64ca9baba26aeb917efb5d125b4c893d
rakib <phi.rakib@gmail.com>
2021-06-12 22:56:52 +0600

    added todo insert form

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    <h3>Add Todo</h3>
+    <form>
+      <input type="text" v-model="title" />
+      <input type="submit" value="Add" />
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
   methods:{},
   computed:{},
   created() {
